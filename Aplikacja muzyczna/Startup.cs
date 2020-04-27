58027a28ac3c7236f165c437c4e7b2f34040b17c
Wojciech_Wróblewski <230218@student.pwr.edu.pl>
2020-04-27 16:24:42 +0200

    Add project files.

---
+++ Aplikacja muzyczna/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Aplikacja_muzyczna.Startup))]
+namespace Aplikacja_muzyczna
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
