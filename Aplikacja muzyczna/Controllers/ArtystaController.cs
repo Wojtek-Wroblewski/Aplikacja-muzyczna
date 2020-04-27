df0861e810eb28dd5e171868d1777fbb626574f9
Wojciech_Wróblewski <230218@student.pwr.edu.pl>
2020-04-27 16:27:27 +0200

    New Project

---
+++ Aplikacja muzyczna/Controllers/ArtystaController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Aplikacja_muzyczna.Controllers
+{
+    public class ArtystaController : Controller
+    {
+        // GET: Artysta
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Artysta/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Artysta/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Artysta/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Artysta/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Artysta/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Artysta/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Artysta/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
