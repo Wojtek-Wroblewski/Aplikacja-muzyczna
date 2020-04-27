88502e2f2b4fa1221c2209e8857b6f841eed7c08
Wojciech_Wróblewski <230218@student.pwr.edu.pl>
2020-04-27 17:57:37 +0200

    Dodano Modele Artysty, edycja form-horizontal

--- Aplikacja muzyczna/DBConnect/DataAccess.cs
+++ Aplikacja muzyczna/DBConnect/DataAccess.cs
@@ -1,11 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Aplikacja_muzyczna.DBConnect
 {
     public class DataAccess
     {
+
+        public static string GetConnectionString(string connectionName = "Aplikacja muzyczna")
+        {
+            return "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=aspnet - Aplikacja muzyczna - 20200427042437;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        }
+
+        public static List<T> LoadData<T>(string sql)
+        {
+            using (IDbConnection cnn = new SqlConnection(GetConnectionString()))
+            {
+                return cnn.Query<T>(sql).ToList();
+            }
+        }
+
+
+
+        public static int SaveData<T>(string sql, T data)
+        {
+
+            using (IDbConnection cnn = new SqlConnection(GetConnectionString()))
+            {
+                return cnn.Execute(sql, data);
+            }
+
+        }
+
     }
 }
---
+++ Aplikacja muzyczna/Models/ArtystaModels.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Drawing;
+using System.Web;
+
+namespace Aplikacja_muzyczna.Models
+{
+    public class DodajArtysta
+    {
+
+        [Display(Name = "Podaj nazwisko lub nazwę zespołu")]
+        [Required(ErrorMessage = "Pole nie może być puste")]
+        public string Nazwa1 { get; set; }
+        [Display(Name = "Podaj imię lub skrót od nazwy zespołu")]
+        public string Nazwa2 { get; set; }
+        [Display(Name = "Podaj datę urodzenia artysty/najmłodszego członka")]
+        [Required(ErrorMessage = "Pole nie może być puste")]
+        [DataType(DataType.Date)]
+        public DateTimeOffset DataUr { get; set; }
+        [Display(Name = "Wybierz zdjęcie")]
+        public byte[] Zdj { get; set; }
+        [Display(Name = "Dodatkowe informacje (np. wykonywane gatunki)")]
+        public string Uwaga { get; set; }
+        public HttpPostedFileBase UserProfilePicture { get; set; }
+        public int ArtId { get; set; }
+
+    }
+
+    public class WyświetlArtysta
+    {
+
+        [Display(Name = "Podaj nazwisko lub nazwę zespołu")]
+        [Required(ErrorMessage = "Pole nie może być puste")]
+        public string Nazwa1 { get; set; }
+        [Display(Name = "Podaj imię lub skrót od nazwy zespołu")]
+        public string Nazwa2 { get; set; }
+        [Display(Name = "Podaj datę urodzenia artysty/najmłodszego członka")]
+        [Required(ErrorMessage = "Pole nie może być puste")]
+        [DataType(DataType.Date)]
+        public DateTimeOffset DataUr { get; set; }
+        [Display(Name = "Wybierz zdjęcie")]
+        public byte[] Zdj { get; set; }
+        [Display(Name = "Dodatkowe informacje (np. wykonywane gatunki)")]
+        public string Uwaga { get; set; }
+        public HttpPostedFileBase UserProfilePicture { get; set; }
+        public int ArtId { get; set; }
+
+    }
+
+    public class edytujArtysta
+    {
+
+        [Display(Name = "Podaj nazwisko lub nazwę zespołu")]
+        [Required(ErrorMessage = "Pole nie może być puste")]
+        public string Nazwa1 { get; set; }
+        [Display(Name = "Podaj imię lub skrót od nazwy zespołu")]
+        public string Nazwa2 { get; set; }
+        [Display(Name = "Podaj datę urodzenia artysty/najmłodszego członka")]
+        [Required(ErrorMessage = "Pole nie może być puste")]
+        [DataType(DataType.Date)]
+        public DateTimeOffset DataUr { get; set; }
+        [Display(Name = "Wybierz zdjęcie")]
+        public byte[] Zdj { get; set; }
+        [Display(Name = "Dodatkowe informacje (np. wykonywane gatunki)")]
+        public string Uwaga { get; set; }
+        public HttpPostedFileBase UserProfilePicture { get; set; }
+        public int ArtId { get; set; }
+
+    }
+
+}
